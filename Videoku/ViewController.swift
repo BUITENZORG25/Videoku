0385dbcee6c838003ac7da1a85bd830e9a6ae061
User <user@kerjalemburbagaikuda.local>
2018-10-19 07:59:06 +0700

    Initial Commit

---
+++ Videoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Videoku
+//
+//  Created by User on 19/10/18.
+//  Copyright © 2018 cahaya Ilahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
